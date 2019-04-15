2413300db215ae11fab0724aa98971105a341073
Andrew Waller <andrewhwaller@gmail.com>
2019-04-15 02:10:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,45 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT,
+      breed TEXT
+    );
+    SQL
+  end
+
+  def self.drop_table
+
+  end
+
+  def save
+
+  end
+
+  def create
+    
+  end
+
+  def self.find_by_id
+
+  end
+
+  def self.find_or_create_by
+    
+  end
+
+  def new_from_db
+
+  end
+
+  def find_by_name
+
+  end
+
+  def update
+    
+  end
 end
